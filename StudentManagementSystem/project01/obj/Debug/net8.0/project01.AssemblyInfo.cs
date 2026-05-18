60253823066a4267414a7001864349b69677aef2
Malakaka <malakalsenine@gmail.com>
2026-05-18 15:27:36 +0400

    Create console project and install Entity Framework Core packages

--- StudentManagementSystem/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
+++ StudentManagementSystem/project01/obj/Debug/net8.0/project01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69083c6d84949dd0b381660450062985bee1dd48")]
 [assembly: System.Reflection.AssemblyProductAttribute("project01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
